13d0968c86e722e394a7fe77c135dceef706b1c0
Ioshua-N <ioshuanoliveira@gmail.com>
2023-09-19 18:11:43 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,15 @@
 // Declara as variáveis de latitude e longitude no escopo global
 var latitude;
 var longitude;
+var map;
+var coordenadas;
+var marker;
 
 // Função para receber latitude e longitude
-if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(function (position) {
+if ("geolocation" in navigator) 
+{
+    navigator.geolocation.getCurrentPosition(function (position) 
+    {
         latitude = position.coords.latitude;
         longitude = position.coords.longitude;
         
@@ -12,48 +17,70 @@ if ("geolocation" in navigator) {
         initMap();
         readJSON();
     });
-} else {
+} 
+else 
+{
     // Problema na localização
     alert("Não foi possível usar a localização.");
 }
 
 // Função para inicializar o mapa
-function initMap() {
+function initMap() 
+{
     // Verifica se as coordenadas estão definidas
-    if (typeof latitude !== 'undefined' && typeof longitude !== 'undefined') {
+    if (typeof latitude !== 'undefined' && typeof longitude !== 'undefined') 
+    {
         // Cria um objeto LatLng com as coordenadas
-        var coordenadas = new google.maps.LatLng(latitude, longitude);
+        coordenadas = new google.maps.LatLng(latitude, longitude);
 
         // Opções de configuração do mapa
-        var mapOptions = {
+        var mapOptions = 
+        {
             center: coordenadas,
             zoom: 15, // Nível de zoom (0 a 21)
         };
 
         // Cria o objeto do mapa
-        var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
         // Adiciona um marcador para indicar a localização
-        var marker = new google.maps.Marker({
+        marker = new google.maps.Marker(
+        {
             position: coordenadas,
             map: map,
             title: 'Você está aqui'
         });
-    } else {
+    } 
+    else 
+    {
         alert("Latitude e/ou longitude não definidas.");
     }
 }
 
-function readJSON()
+function readJSON() 
 {
     fetch('academiadacidade.json')
         .then(response => response.json())
-        .then(data => 
-            {
-                console.log(data);
-            })
-        .catch(error => 
-            {
-                console.error('Erro ao buscar dados:', error);
+        .then(data => {
+            data.forEach(element => {
+                // Create a LatLng object with the coordinates
+                const coordenadas = new google.maps.LatLng(element.latitude, element.longitude);
+
+                // Create a marker
+                const marker = new google.maps.Marker({
+                    position: coordenadas,
+                    map: map, // Make sure 'map' is defined in your code
+                    title: element.nome_oficial
+                });
+
+                // Optionally, you can add an event listener to the marker
+                marker.addListener('click', () => {
+                    // Handle marker click event here
+                    console.log(`Marker clicked: ${element.nome_oficial}`);
+                });
             });
+        })
+        .catch(error => {
+            console.error('Erro ao buscar dados:', error);
+        });
 }
